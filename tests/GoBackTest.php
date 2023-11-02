595f86575ff4fab6a02344adf7522fb133e51371
Chris Yates <chris@clyates.plus.com>
2023-11-02 21:51:00 +0000

    Add GoBackTest.php

---
+++ tests/GoBackTest.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * @copyright Copyright © 2023 BeastBytes - All rights reserved
+ * @license BSD 3-Clause
+ */
+
+declare(strict_types=1);
+
+namespace BeastBytes\Yii\Middleware\Tests;
+
+use BeastBytes\Yii\Middleware\GoBack;
+use DG\BypassFinals;
+use HttpSoft\Message\Response;
+use HttpSoft\Message\ResponseFactory;
+use HttpSoft\Message\ServerRequest;
+use HttpSoft\Message\Uri;
+use Psr\Http\Message\ServerRequestInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Server\RequestHandlerInterface;
+use Yiisoft\Http\Method;
+use Yiisoft\Router\CurrentRoute;
+use Yiisoft\Session\SessionInterface;
+
+class GoBackTest extends TestCase
+{
+    private const IGNORED_ROUTE = 'ignoredRoute';
+    private const NOT_IGNORED_ROUTE = 'notIgnoredRoute';
+    private const NOT_IGNORED_URI = 'https://example.com/not-ignored';
+
+    private array $session = [];
+
+    public static function setUpBeforeClass(): void
+    {
+        BypassFinals::enable();
+    }
+
+    public function testRouteIsIgnoredRoute(): void
+    {
+        $currentRoute = $this
+            ->createCurrentRoute(self::IGNORED_ROUTE)
+        ;
+        $session = $this
+            ->createSession()
+        ;
+
+        $goBack = new GoBack($currentRoute, $session);
+        $goBack->addIgnoredRoute(self::IGNORED_ROUTE);
+
+        $goBack->process($this->createRequest(), $this->createHandler());
+
+        $this->assertEmpty($session->get(GoBack::URL_PARAM));
+    }
+
+    public function testRouteIsNotIgnoredRoute(): void
+    {
+        $currentRoute = $this
+            ->createCurrentRoute(self::NOT_IGNORED_ROUTE, self::NOT_IGNORED_URI)
+        ;
+        $session = $this
+            ->createSession()
+        ;
+
+        $goBack = new GoBack($currentRoute, $session);
+        $goBack->addIgnoredRoute(self::IGNORED_ROUTE);
+
+        $goBack->process($this->createRequest(), $this->createHandler());
+
+        $this->assertNotEmpty($session->get(GoBack::URL_PARAM));
+        $this->assertSame(self::NOT_IGNORED_URI, $session->get(GoBack::URL_PARAM));
+    }
+
+    private function createRequest(): ServerRequestInterface {
+        return new ServerRequest(
+            cookieParams: [],
+            queryParams: [],
+            method: Method::GET,
+            uri: '/',
+            headers: [],
+        );
+    }
+
+    private function createHandler(): RequestHandlerInterface
+    {
+        return new class () implements RequestHandlerInterface {
+            public function handle(ServerRequestInterface $request): Response
+            {
+                return new Response();
+            }
+        };
+    }
+
+    private function createCurrentRoute(string $route, string $uri = ''): CurrentRoute
+    {
+        $currentRoute = $this->createMock(CurrentRoute::class);
+        $currentRoute
+            ->method('getName')
+            ->willReturn($route);
+        $currentRoute
+            ->method('getUri')
+            ->willReturn(new Uri($uri));
+
+        return $currentRoute;
+    }
+
+    private function createSession(): SessionInterface
+    {
+        $session = $this->createMock(SessionInterface::class);
+        $session
+            ->method('set')
+            ->willReturnCallback(function ($name, $value) {
+                $this->session[$name] = $value;
+            });
+
+        $session
+            ->method('get')
+            ->willReturnCallback(fn ($name) => $this->session[$name]);
+
+        return $session;
+    }
+}
